b7816875ab0363fb7c4e74ecb6cd22378f0c3c3c
Elie Daou <elie.daw@gmail.com>
2016-02-17 13:22:42 +0200

    - Fixed recursive Fibonacci - Added a test case

--- 2016-02-17-even-fibonacci-numbers/src/main/java/Fibonacci.java
+++ 2016-02-17-even-fibonacci-numbers/src/main/java/Fibonacci.java
@@ -1,9 +1,11 @@
 public class Fibonacci {
 
    public static int compute(int n) {
-      int fibb = 0;
-      if (n <= 2) {
-         return n;
+      if (n ==0 ) {
+         return 0;
+      }
+      if (n == 1) {
+         return 1;
       }
 
       return compute(n - 1) + compute(n - 2);
--- 2016-02-17-even-fibonacci-numbers/src/test/java/FibonacciTest.java
+++ 2016-02-17-even-fibonacci-numbers/src/test/java/FibonacciTest.java
@@ -1,4 +1,5 @@
 import org.fest.assertions.api.Assertions;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class FibonacciTest {
@@ -6,6 +7,18 @@ public class FibonacciTest {
    @Test
    public void if_input_is_1_should_return_1(){
       Assertions.assertThat(Fibonacci.compute(1)).isEqualTo(1);
+   }
+
+
+   @Test
+   public void if_input_is_10(){
+      Assertions.assertThat(Fibonacci.compute(10)).isEqualTo(55);
+   }
+
+   @Ignore
+   @Test
+   public void if_input_is_4000000(){
+      Assertions.assertThat(Fibonacci.compute(4000000)).isEqualTo(5);
    }
 
 }
