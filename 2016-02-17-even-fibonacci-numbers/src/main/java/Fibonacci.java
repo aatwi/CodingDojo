ef3ebdada8f91496ccee1a92b194e849ad386c33
Ahmad Atwi <ahmad.atwi@murex.com>
2016-02-17 14:07:01 +0200

    Commit to master

--- 2016-02-17-even-fibonacci-numbers/src/main/java/Fibonacci.java
+++ 2016-02-17-even-fibonacci-numbers/src/main/java/Fibonacci.java
@@ -20,5 +20,25 @@ public class Fibonacci {
       return c;
    }
 
+   public static long computeLessThanFourMillion()
+   {
+      int iteration=0;
+      long result = 0;
+      while (result<4000000)
+      {
+         result=compute(iteration);
+         iteration++;
+
+      }
+       return result;
+   }
+
+   public static boolean checkeven(int n)
+   {
+      if (n%2 == 0)
+         return true;
+      else
+      return false;
+   }
 
 }
--- 2016-02-17-even-fibonacci-numbers/src/test/java/FibonacciTest.java
+++ 2016-02-17-even-fibonacci-numbers/src/test/java/FibonacciTest.java
@@ -4,18 +4,27 @@ import org.junit.Test;
 public class FibonacciTest {
 
    @Test
-   public void if_input_is_1_should_return_1(){
+   public void if_input_is_1_should_return_1() {
       Assertions.assertThat(Fibonacci.compute(1)).isEqualTo(1);
    }
 
    @Test
-   public void if_input_is_10(){
+   public void if_input_is_10() {
       Assertions.assertThat(Fibonacci.compute(10)).isEqualTo(55);
    }
 
+   @Test
+   public void check_if_even_input_is_10() {
+      Assertions.assertThat(Fibonacci.checkeven(10)).isTrue();
+   }
 
    @Test
-   public void if_input_is_4000000(){
+   public void check_if_even_input_is_5() {
+      Assertions.assertThat(Fibonacci.checkeven(5)).isFalse();
+   }
+
+   @Test
+   public void if_input_is_4000000() {
       Assertions.assertThat(Fibonacci.compute(4000000)).isEqualTo(6558868233897966651L);
    }
 
